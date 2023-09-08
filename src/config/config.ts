161e96618149a24a8f46b2ad8a7a7e4fd3f70a41
lebrancconvas <lebrancconvas@gmail.com>
2023-09-08 14:44:33 +0700

    ✨ feat: add types

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,6 @@
-export const config = {
+import { IConfig } from "../@types";
+
+export const config: IConfig = {
   width: window.innerWidth,
   height: window.innerHeight,
   backgroundColor: 0x000000

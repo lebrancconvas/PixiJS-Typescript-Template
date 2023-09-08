859bfd30a41ffe8366bcb14e402e3feab85b0fc1
lebrancconvas <lebrancconvas@gmail.com>
2023-09-08 16:52:23 +0700

    ✨ feat: add game loop

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
-import { app } from './renderer';
+import { PIXI, app } from './renderer';
 import { config } from './config';
 
 document.body.appendChild(app.view);
 app.renderer.background.color = config.backgroundColor;
 
+// Setup.
+
 // Init.
 
 // Game Loop.
+app.ticker.add((deltaTime: number) => loop(deltaTime));
+
+function loop(deltaTime: number) {
+  // Update.
+
+  // Render.
+
+
+  console.log(`Delta Time: ${deltaTime}`);
+};
